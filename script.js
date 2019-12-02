c7ce4a22097ce53d96f09f0382de7a5f40e38773
linaLeinartaite <lleinartaite@yahoo.com>
2019-12-02 17:38:57 +0200

    last before class

---
+++ script.js
@@ -0,0 +1,34 @@
+
+var closeBtnS = document.getElementsByClassName('closeBtnS')[0];
+var closeBtnL = document.getElementsByClassName('closeBtnL')[0];  /*this gives array of elements with 
+specified class name, so you have to have index number even if there is only one button */
+
+var formS = document.getElementById("signInForm");
+var formL = document.getElementById("logInForm");
+
+var btnLastS = document.getElementsByClassName("btnLastS")[0];
+var btnLastL = document.getElementsByClassName("btnLastL")[0];
+
+closeBtnS.addEventListener("click", closeFormS);
+closeBtnL.addEventListener("click", closeFormL);
+
+function closeFormS() {
+    formS.style.display="none";
+ }
+function closeFormL() {
+   formL.style.display="none";
+}
+
+btnLastS.addEventListener("click", openFormS);
+btnLastL.addEventListener("click", openFormL);
+
+function openFormL() {
+    formS.style.display="none";
+    formL.style.display="inline-block";
+ }
+ 
+ function openFormS() {
+     formL.style.display="none";
+     formS.style.display="inline-block";
+  }
+ 
